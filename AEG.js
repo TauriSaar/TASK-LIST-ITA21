61ae49e9313dd009dce5a81f1b5ced1265e49673
Tauri <tauri.saar@voco.ee>
2022-09-23 08:04:48 +0300

    sumea

---
+++ AEG.js
@@ -0,0 +1,21 @@
+const today = new Date()
+
+let val
+
+val = today
+val = today.getMonth() // 0 - Jaanuar
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getMinutes()
+val = today.getTime()
+
+val = new Date("09/22/2021")
+val = new Date('September 23 2022')
+val.setDate(23)
+val.setFullYear(2023)
+
+
+
+console.log(val)
---
+++ TINGIMUSLAUSED.js
@@ -0,0 +1,71 @@
+/*
+if(condition) {
+    // if statments
+} else {
+    // else statments
+}
+
+== equal   -> ===
+!= not equal -> !==
+ */
+
+let personID;
+
+if(typeof personID === 'undefined'){
+    console.log('id is not correct')
+} else {
+    console.log('id is ${personID}')
+}
+
+/*
+if(condition) {
+    // if statments
+} else if(condition){
+    // else statments
+} else {
+    else condition
+}
+ */
+
+if(color === 'red'){
+    console.log('STOP!')
+} else if(color === 'yellow'){
+    console.log('Attention!')
+}else if(color === 'green'){
+    console.log('Go!')
+} else {
+    console.log('Error!')
+}
+
+const name = 'Kadi'
+const age = 40
+
+if(age > 0 && age <= 12){
+    console.log('${name} on laps')
+} else if(age > 12 && age <= 118){
+    console.log('${name} on nooruk')
+} else {
+    console.log('${name} on täiskasvanud')
+}
+
+let id = 5
+
+if(id === 100){
+    console.log('Ok')
+} else {
+    console.log('Not')
+}
+
+// ? - ternari
+console.log(id === 100 ? 'Ok' : 'Not')
+
+//switch
+const color = 'red'
+
+switch (color){
+    case "red":
+        console.log('Stop!)
+            break;
+}
+
+console.log(personID)
